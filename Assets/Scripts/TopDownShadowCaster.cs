e5933d1ab827f19a46ce6388a6a70924aa86bd1b
Jakub Wiśniewski <wisniaN7@gmail.com>
2024-01-28 12:47:17 +0100

    Player Ring Fix

--- Assets/Scripts/TopDownShadowCaster.cs
+++ Assets/Scripts/TopDownShadowCaster.cs
@@ -7,7 +7,7 @@ public class TopDownMeshCaster : MonoBehaviour {
 	[SerializeField] private Transform playerRing;
 
 	private void FixedUpdate() {
-		Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 100f);
+		Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, out RaycastHit hit, 100f, LayerMask.GetMask("Terrain"));
 		playerRing.position = hit.collider ? hit.point : transform.position;
 	}
 }
