23ea19e49864dd1ab8c4e0b63a2c927dc8428bb4
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2024-01-28 12:18:07 +0100

    loop lag fix

--- Assets/Scripts/music setter scripts/levelmusicsetter.cs
+++ Assets/Scripts/music setter scripts/levelmusicsetter.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class levelmusicsetter : MonoBehaviour
 {
-    public AudioClip levelmusicWarmup;
     public AudioClip levelmusicMain;
     
     // Start is called before the first frame update
     void Start()
     {
-        SoundVisualizer.Instance.AddToQueue(levelmusicWarmup);
         SoundVisualizer.Instance.AddToQueue(levelmusicMain);
         SoundVisualizer.Instance.StartPlayingQueue();
     }
